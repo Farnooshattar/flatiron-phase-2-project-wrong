8847f5bf9a68d029905f385d668a47aa3988ce29
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-06-26 15:10:36 -0400

    Creates LoginShowModal.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Counter from "../Counter";
-import LoginShowmodal from "../LoginShowmodal";
+import LoginShowModal from "../LoginShowModal";
 
 function HomePage() {
   return (
     <div>
       <div>
         <Counter />
-        <LoginShowmodal />
+        <LoginShowModal />
       </div>
       <img
         src={require("../images/679db61ec05330a482f8ca16488cf145.gif")}
